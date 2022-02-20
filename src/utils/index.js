8c436b399abe2112470522f319580cd756e96374
sww <shenwenwu@enn.cn>
2022-02-20 20:03:57 +0800

    feat: dashboard添加一些内容

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+var getRandomColor = function() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+export {
+  getRandomColor
+}
--- src/views/subViews/dashboard.vue
+++ src/views/subViews/dashboard.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="dashboard-container">
     <div class="statistics-card">
-      <div class="sc-1">statistics-card1</div>
-      <div>statistics-card2</div>
-      <div>statistics-card3</div>
-      <div>statistics-card4</div>
+      <div v-for="(item, index) in statisticsCardList" :key="index" class="statistics-list-item">
+        <div class="sc-header"></div>
+        <div class="sc-content"></div>
+        <div class="sc-footer"></div>
+      </div>
     </div>
     <div class="type-card">
-      <div>type-card1</div>
-      <div>type-card2</div>
-      <div>type-card3</div>
-      <div>type-card4</div>
-      <div>type-card5</div>
-      <div>type-card6</div>
-      <div>type-card7</div>
-      <div>type-card8</div>
+      <div v-for="(item, index) in typeCardList" :key="index" class="tc-item">
+        <iconfont :icon-name="item.icon" :iconColor="item.iconColor"></iconfont>
+        {{ item.title }}
+      </div>
     </div>
     <div class="chart-card">
       <el-tabs v-model="activeName" @tab-click="handleClick">
@@ -30,10 +27,62 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import FlowTrend from '@/components/dashboard/FlowTrend.vue'
 import Visits from '@/components/dashboard/Visits.vue'
+import iconfont from '@/components/iconfont/index.vue'
 const activeName = ref('flowTrend')
+const statisticsCardList = reactive([{}, {}, {}, {}])
+const typeCardList = reactive([
+  {
+    type: '',
+    iconColor: '#69c0ff',
+    icon: 'icon-user',
+    title: '用户'
+  },
+  {
+    type: '',
+    iconColor: '#69c0ff',
+    icon: 'icon-fenxi',
+    title: '分析'
+  },
+  {
+    type: '',
+    iconColor: '#ffa67c',
+    icon: 'icon-shangpin',
+    title: '商品'
+  },
+  {
+    type: '',
+    iconColor: '#ba8bed',
+    icon: 'icon-dingdan',
+    title: '订单'
+  },
+  {
+    type: '',
+    iconColor: '#ffd666',
+    icon: 'icon-piaoju_shouju',
+    title: '票据'
+  },
+  {
+    type: '',
+    iconColor: '#7ae1db',
+    icon: 'icon-xiaoxi',
+    title: '消息'
+  },
+  {
+    type: '',
+    iconColor: '#ff8dc4',
+    icon: 'icon-biaoqian',
+    title: '标签'
+  },
+  {
+    type: '',
+    iconColor: '#ffcb82',
+    icon: 'icon-setting1',
+    title: '配置'
+  }
+])
 const handleClick = (tab, event) => {
   console.log(tab, event)
 }
@@ -44,18 +93,51 @@ const handleClick = (tab, event) => {
   padding: 0 10px;
   box-sizing: border-box;
   .statistics-card {
-    margin-top: 1rem;
     display: grid;
     height: 200px;
     grid-template-columns: repeat(4, minmax(0px, 1fr));
     gap: 8px 12px;
+    .statistics-list-item {
+      display: flex;
+      flex-direction: column;
+      > div {
+        width: 100%;
+      }
+      .sc-header {
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        font-size: 16px;
+        padding: 12px 16px 12px 16px;
+        box-sizing: border-box;
+        height: 48px;
+      }
+      .sc-content {
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        font-size: 16px;
+        padding: 12px 16px 12px 16px;
+        box-sizing: border-box;
+        height: 108px;
+      }
+      .sc-footer {
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        font-size: 16px;
+        padding: 12px 16px 12px 16px;
+        box-sizing: border-box;
+        height: 48px;
+      }
+    }
   }
   .statistics-card,
   .type-card {
     > div {
-      display: grid;
+      display: flex;
       align-items: center;
-      justify-items: center;
+      justify-content: center;
       background-color: #fff;
     }
   }
@@ -65,6 +147,15 @@ const handleClick = (tab, event) => {
     height: 80px;
     grid-template-columns: repeat(8, minmax(0px, 1fr));
     gap: 8px 12px;
+
+    .tc-item {
+      flex-direction: column;
+
+      i {
+        width: 2em;
+        height: 2em;
+      }
+    }
   }
 
   .chart-card {

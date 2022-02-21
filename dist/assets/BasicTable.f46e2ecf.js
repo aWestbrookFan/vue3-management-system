fb0f24784086c9d423a6b42add9d37c4a9514fec
sww <shenwenwu@enn.cn>
2022-02-21 10:08:59 +0800

    feat: 添加dist

---
+++ dist/assets/BasicTable.f46e2ecf.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a3a7b308.js";import{o as a,a as _}from"./vendor.eb69b1b6.js";const c={},s={class:"baiscTable-container"};function t(o,r){return a(),_("div",s,"\u57FA\u7840\u8868\u683C")}var d=e(c,[["render",t],["__scopeId","data-v-df362734"]]);export{d as default};

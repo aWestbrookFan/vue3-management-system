4ce6a47aa4cddad2de2d2d1db13e47bd2f6dc931
sww <shenwenwu@enn.cn>
2022-02-22 11:02:21 +0800

    feat: 添加404页面、copy一些svg图

--- src/views/subViews/404.vue
+++ src/views/subViews/404.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>404</div>
+  <div class="page-container">
+    <div class="text-center">
+      <img src="@/assets/exception/404.svg" alt="" />
+    </div>
+    <div class="text-center">
+      <h1>抱歉，你访问的页面不存在</h1>
+      <el-button type="primary" @click="goHome">回到首页</el-button>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+function goHome() {
+  router.push('/')
+}
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+.page-container {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  overflow: hidden;
+  justify-content: center;
+}
+.text-center {
+  img {
+    width: 350px;
+    height: auto;
+  }
+  h1 {
+    color: #666;
+    padding: 20px 0;
+  }
+}
+</style>
